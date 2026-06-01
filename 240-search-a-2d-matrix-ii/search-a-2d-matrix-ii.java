bb855a04ad115416d43f70eb828f1e37199fb23e
Vansh Gupta <156058230+VanshGupta77@users.noreply.github.com>
2026-06-01 21:08:42 +0530

    Time: 17 ms (6.71%) | Memory: 50 MB (9.57%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+          // Search in Sorted Matrix (Rows sorted , columns sorted , Time Complexity (O(n+m)))
+    
+                // Case where Starting Point is Top-Right Corner
+                int row =0 , col= matrix[0].length-1;
+                while(row < matrix.length && col >= 0){
+                    if(matrix[row][col] == target){
+                        System.out.println("Found Target at (" + row + "," + col + ")");
+                        return true;
+                    }
+                    else if(target < matrix[row][col]){
+                        col--;
+                    }
+                    else{
+                        row++;
+                    }
+                }
+                System.out.println("Target not Found");
+                return false;
+            }
+    }

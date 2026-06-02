b341d0fd29b478f3d7c64131bffeac7134ea43a8
Vansh Gupta <156058230+VanshGupta77@users.noreply.github.com>
2026-06-03 02:00:18 +0530

    Time: 3 ms (66.80%) | Memory: 47 MB (39.06%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,24 +1,20 @@
 class Solution {
     public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-        int land  = calc(landStartTime, landDuration, waterStartTime, waterDuration);
-        int water = calc(waterStartTime, waterDuration, landStartTime, landDuration);
-        return Math.min(land, water);
-    }
+    //    To get Minimun Overall Time
+        int minFinish = Integer.MAX_VALUE;
+        int n = landStartTime.length;
+        int m = waterStartTime.length;
 
-    private int calc(int[] s1, int[] t1, int[] s2, int[] t2) {
-        // Find earliest finish time among all first-category rides
-        int minEnd = Integer.MAX_VALUE;
-        for (int i = 0; i < s1.length; i++) {
-            minEnd = Math.min(minEnd, s1[i] + t1[i]);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+            // If we go land Slides 1st
+                int plan1 = Math.max(landStartTime[i] + landDuration[i], waterStartTime[j]) + waterDuration[j];
+            // If we go Water Slides 1st   
+                int plan2 = Math.max(waterStartTime[j] + waterDuration[j], landStartTime[i]) + landDuration[i];
+            // To Calculate Minimum finish Time out of both Plans
+                minFinish = Math.min(minFinish, Math.min(plan1, plan2));
+            }
         }
-
-        // Find earliest finish time among all second-category rides
-        // max(minEnd, s2[i]) = actual boarding time (wait if ride not open yet)
-        int finish = Integer.MAX_VALUE;
-        for (int i = 0; i < s2.length; i++) {
-            finish = Math.min(finish, Math.max(minEnd, s2[i]) + t2[i]);
-        }
-
-        return finish;
+        return minFinish;
     }
 }

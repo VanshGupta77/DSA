a2470ce6eb1796edd58f34cec5869b9bd4ede382
Vansh Gupta <156058230+VanshGupta77@users.noreply.github.com>
2026-06-24 03:28:42 +0530

    Time: 1 ms (96.45%) | Memory: 42.7 MB (17.62%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,13 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if (n <= 0) return false;
+        if (n<=0) return false;
 
-        while (n > 1) {
-            // Search for odd number 
-            if (n % 2 != 0) return false;  
-            n /= 2;
-        }
-
-        return true;
+        return (n & (n-1)) ==0;
     }
 }

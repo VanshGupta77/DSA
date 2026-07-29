b0601938ac6a338ea0fe3e1bb3c8a757cb280c84
Vansh Gupta <156058230+VanshGupta77@users.noreply.github.com>
2026-07-30 01:12:24 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (57.74%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double myPow(double x, int n) {
+        // return x * myPow(x , n-1);
+        return myPow(x , (long)n);
+    }
+       private double myPow(double x, long n){
+       if(n ==0){
+            return 1;
+        }
+         if (n < 0) {
+            return 1.0 / myPow(x, Math.abs(n)); //Negative Power
+        }
+        // int halfPowersq = optmPower(a, n/2) * optmPower(a, n/2); (More Time Complexity)
+        double halfPower = myPow(x, n/2);
+        double halfPowerSq = halfPower * halfPower;
+        
+        //N is odd
+        if(n %2 != 0){
+            halfPowerSq = x * halfPowerSq;
+        }
+        return halfPowerSq;
+       }
+}

f817f1f6ba3253e230229ec6b898115e2346e6d4
Vansh Gupta <156058230+VanshGupta77@users.noreply.github.com>
2026-08-01 02:56:40 +0530

    Time: 1 ms (94.39%) | Memory: 43.5 MB (52.90%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -4,11 +4,11 @@ class Solution {
         for(int i=0; i<numRows; i++){
             List<Integer> row = new ArrayList<>();
             for(int j=0; j<= i; j++){
-                if(j == 0 || j == i){
+                if(j == 0 || j == i){ //when index becomes 0 or 1 for new row
                     row.add(1);
                 }
                 else{
-                    int var = result.get(i - 1).get(j - 1) +result.get(i - 1).get(j);
+                    int var = result.get(i - 1).get(j - 1) +result.get(i - 1).get(j); //To add 1 on previous row(i - 1) and add 1 on both sides of previous row (j - 1 ) , (j)
                     row.add(var);
                 }
             }

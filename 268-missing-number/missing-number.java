c6d30324a525c7eacdf91fd4b6178395e9f860c2
Vansh Gupta <156058230+VanshGupta77@users.noreply.github.com>
2026-08-22 01:34:07 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (33.13%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,15 +1,14 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int xor=0;
-        int n = nums.length;
-
-        for(int i=0; i<=n; i++){ //take XOR from range 0 to n
-            xor = xor ^ i;
+        int total =0;
+        int sum=0;
+        for(int ele: nums){
+            sum+=ele;
         }
-
-        for(int i=0 ; i<nums.length; i++){// take XOR of Actual Array Elements
-            xor = xor^nums[i];
+        for(int i=0; i<=nums.length; i++){
+            total += i;
         }
-        return xor;
+        total = total - sum;
+        return total;
     }
 }
